5431573859718688ae326a483fa1cce53fe299f1
Paul Gadi <paulgadi@gmail.com>
2020-12-06 16:54:28 +0800

    disable notifications

--- docs/pwa.js
+++ docs/pwa.js
@@ -11,21 +11,21 @@ if(enablePWA) {
         })
     };
     
-	// NOTIFICATIONS TEMPLATE
-	Notification.requestPermission().then(function(result) {
-		if(result === 'granted') {
-			exampleNotification();
-		}
-	});
-	function exampleNotification() {
-		var notifTitle = 'Booter';
-		var notifBody = 'Zero-config template for web games.';
-		var notifImg = 'img/icons/icon-512.png';
-		var options = {
-			body: notifBody,
-			icon: notifImg
-		}
-		var notif = new Notification(notifTitle, options);
-		setTimeout(exampleNotification, 30000);
-	}
+	// // NOTIFICATIONS TEMPLATE
+	// Notification.requestPermission().then(function(result) {
+	// 	if(result === 'granted') {
+	// 		exampleNotification();
+	// 	}
+	// });
+	// function exampleNotification() {
+	// 	var notifTitle = 'Booter';
+	// 	var notifBody = 'Zero-config template for web games.';
+	// 	var notifImg = 'img/icons/icon-512.png';
+	// 	var options = {
+	// 		body: notifBody,
+	// 		icon: notifImg
+	// 	}
+	// 	var notif = new Notification(notifTitle, options);
+	// 	setTimeout(exampleNotification, 30000);
+	// }
 }
